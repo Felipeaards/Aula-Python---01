1d0635d0436ac0d1279d25a0a6d03de831afa622
Felipeaards <felipeaards@gmail.com>
2026-09-03 20:24:46 -0300

    Renomeação do arquivo

---
+++ Exemplo de tipos de variaveis.py
@@ -0,0 +1,32 @@
+nome = input("Digite seu nome")
+print(nome, type(nome))
+
+idade = input("Digite sua idade")
+print(idade, type(idade))
+
+#Toda informação enviada no terminal é uma String
+
+
+#Para resolver isso, usamos ma função de conversão
+
+#Funções:
+#Número inteiro = int()
+#Números decimais = float()    (Números com vírgula)
+#Booleanos = bool()      (0 - False ou 1 - TrueVerdade)
+idade2 = int(input("Digite sua idade: "))
+print(idade2, type(idade2))
+
+
+altura = float(input("Digite sua altura: "))
+print(altura, type(altura))
+
+
+situacao = bool(input("Digite 0 para sair ou 1 para ficar"))
+print(situacao, type(situacao))
+
+
+
+Verdadeiro = True
+Falso = False
+print(type(Verdadeiro), type(Falso))
+
