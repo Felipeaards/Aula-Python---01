8bec2a955287c438b5cde34f4cca2b5ee489495e
Felipeaards <felipeaards@gmail.com>
2026-09-03 20:04:16 -0300

    Adições Python Aula 03 e 04

---
+++ Brr Brr Patapim.py
@@ -0,0 +1,18 @@
+print("Escolha o seu Brainrot favorito abaixo:")
+print("1: Bailarinna Capputinna")
+print("2: Brr Brr Patapim")
+print("3: Tung Tung Tung Tung Tung Sahur")
+print("4: Moranguito Lapullito")
+print("5: Bombardillo Crocodillo")
+Brainrot = input("Escreva o número do qual você escolheu: ")
+
+if Brainrot == "1":
+    print("Você escolheu Bailarinna Capputinna, a quarta melhor, mas ainda é boa!")
+elif Brainrot == "2":
+    print("Você escolheu Brr Brr Patapim, o terceiro melhor, mas quase bate com o segundo!")
+elif Brainrot == "3":
+        print("Você escolheu Tung Tung Tung Tung Tung Sahur, o melhor de todos!!!")
+elif Brainrot == "4":
+        print("Você escolheu Moranguito Lapullito, mas esse aqui nem existe de verdade kkkkkk")
+elif Brainrot == "5":
+        print("Você escolheu Bombardillo Crocodillo, o segundo melhor!!")
